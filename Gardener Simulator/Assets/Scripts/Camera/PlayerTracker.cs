c79a0568a9bb93e01d3b639c2b646719c16f5e08
stashaKaktus <anna_konoplyanova@mail.ru>
2022-10-30 19:20:49 +0300

    Add Player Tracker

--- Gardener Simulator/Assets/Scripts/Camera/PlayerTracker.cs
+++ Gardener Simulator/Assets/Scripts/Camera/PlayerTracker.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class PlayerTracker : MonoBehaviour
 {
     [SerializeField] Transform _player;
-    [SerializeField] private float _smoothSpeed = 0.125f;
-    [SerializeField] private float _offset;
+    [SerializeField] private float _smoothSpeed = 0.01f;
     
     private PlayerInput _playerInput;
 
@@ -15,9 +14,20 @@ public class PlayerTracker : MonoBehaviour
         _playerInput = new PlayerInput();
     }
 
+    private void OnEnable()
+    {
+        _playerInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerInput.Disable();
+    }
+
     private void Update()
     {
-        Vector3 targetPosition = new Vector3(_player.position.x, _player.position.y, -10);
-        transform.position = Vector3.Lerp(transform.position, targetPosition, 0.01f);
+        Vector3 offset = _playerInput.Player.Move.ReadValue<Vector2>();
+        Vector3 targetPosition = new Vector3(_player.position.x, _player.position.y, -10) + offset;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, _smoothSpeed);
     }
 }
