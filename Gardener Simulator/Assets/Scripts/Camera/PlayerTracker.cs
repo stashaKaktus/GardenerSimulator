61e4f1e7fb69bab111cb76e14f0201cd993e6c94
stashaKaktus <anna_konoplyanova@mail.ru>
2022-11-07 14:11:19 +0300

    Update level

--- Gardener Simulator/Assets/Scripts/Camera/PlayerTracker.cs
+++ Gardener Simulator/Assets/Scripts/Camera/PlayerTracker.cs
@@ -7,7 +7,7 @@ public class PlayerTracker : MonoBehaviour
     [SerializeField] Transform _player;
     [SerializeField] private float _smoothSpeed = 0.125f;
 
-    private void Update()
+    private void LateUpdate()
     {
         Vector3 targetPosition = new Vector3(_player.position.x, _player.position.y, -10); 
         transform.position = Vector3.Lerp(transform.position, targetPosition, _smoothSpeed);
